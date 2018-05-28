37fde4ce499b515ee9e5f3dbdec88fc55c0f9174
jkloshhm <jkloshhm@126.com>
2018-05-28 16:51:01 +0800

    CookDemo上传

--- recipedemo/src/main/java/com/example/recipedemo/RecipeApiService.java
+++ recipedemo/src/main/java/com/example/recipedemo/RecipeApiService.java
@@ -3,6 +3,7 @@ package com.example.recipedemo;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -36,5 +37,18 @@ public interface RecipeApiService {
 
     @GET("query")
     Call<CookMenuByIdBean> getGetMenuFromId(@Query("key") String key, @Query("id") String id);
+
+
+    /**
+     * 根据category获取Android、iOS等干货数据
+     * 接口请求示例：http://gank.io/api/data/{category}/{count}/{page}
+     *
+     * @param category 类别
+     * @param count    条目数目
+     * @param page     页数
+     * @return ResponseBody
+     */
+    @GET("data/{category}/{count}/{page}")
+    Call<ResponseBody> getCategoryData(@Path("category") String category, @Path("count") int count, @Path("page") int page);
 
 }
